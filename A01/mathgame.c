983ebf3f4cb3a96276c18572176ef58183c08299
Fejiroisaacs <fejiro_isaacs@CONQUEROR>
2024-09-05 15:33:12 -0400

    Added lab

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -1,11 +1,33 @@
 /***************************************************
  * mathgame.c
- * Author: 
+ * Author: Oghenefejiro Anigboro 
  * Implements a math game
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-  return 0;
+	int totalGames, x, y, userSol, sol;
+	int correct = 0;
+
+	printf("Welcome to my math game. How many games would you like to play? ");
+	scanf(" %d", &totalGames);
+
+	for (int i = 0; i < totalGames; i++){
+		x = rand() % 10 + 1;
+		y = rand() % 10 + 1;
+		sol = x + y;
+		printf("\n%d + %d = ? ", x, y);
+		scanf(" %d", &userSol);
+		if (userSol == sol){
+			printf("Correct, well done!\n");
+			correct++;
+		} else{
+			printf("Incorrect :), the correct answer is %d\n", sol);
+		}
+		
+	}
+	printf("Game over, you got %d/%d correctly!", correct, totalGames);
+	return 0;
 }
