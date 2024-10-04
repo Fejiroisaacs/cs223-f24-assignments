a76a6447ba91b97b736ff33da1e22f394781ed7a
Oghenefejiro Anigboro <fejiroisaac@gmail.com>
2024-10-04 00:26:09 -0400

    lab complete

--- A05/read_ppm.c
+++ A05/read_ppm.c
@@ -16,23 +16,20 @@ struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
 }
 
 struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
-  printf("Got here\n");
   FILE* ppmData = fopen(filename, "rb");
-  
+    
   if (ppmData == NULL) {
        perror("Error opening file");
        return NULL;
    }
-
+  
   struct ppm_pixel** binary = NULL;
   char buff[1000];
   int index = 0;
   while(fgets(buff, 100, ppmData)){
-    
     if(index == 2){
       *w = (int)buff[0] - (int)'0';
       *h = (int)buff[2] - (int)'0';
-      //break;
     }
     if(index == 3) break;
     index++;
@@ -42,6 +39,10 @@ struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
   
   for(int i = 0; i < *h; i++){
     binary[i] = malloc(sizeof(struct ppm_pixel)* *h);
+    if(binary[i] == NULL){
+      perror("Error allocating memory");
+      return NULL;
+    }
     for(int j = 0; j < *w; j++) fread(&binary[i][j], sizeof(struct ppm_pixel), 1, ppmData);
   }
   
--- A05/test_write.c
+++ A05/test_write.c
@@ -1,7 +1,7 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Oghenefejiro Anigboro 
+ * Date: 10/03/2024
+ * Description: program which tests writing to a binary file
  ---------------------------------------------*/
 
 #include <stdio.h>
@@ -12,9 +12,29 @@
 
 int main(int argc, char** argv) {
   int w, h;
-  struct ppm_pixel* pixels = read_ppm("feep-raw.ppm", &w, &h);
+  struct ppm_pixel** pixels = read_ppm_2d("feep-raw.ppm", &w, &h);
 
   // test writing the file to test.ppm, reload it, and print the contents
+
+  write_ppm_2d("test.ppm", pixels, w, h);
+
+  int w2, h2;
+  struct ppm_pixel** newPixels = read_ppm_2d("test.ppm", &w2, &h2);
+  
+  printf("Testing file test.ppm: %d %d\n", w2, h2);
+  for(int i = 0; i < w; i++){
+    for(int j = 0; j < h; j++){
+      printf("(%hhu,%hhu,%hhu) ", newPixels[i][j].red, newPixels[i][j].green, newPixels[i][j].blue);
+    }
+    printf("\n");
+  }
+
+  for(int i = 0; i < w; i++){
+    free(pixels[i]);
+    free(newPixels[i]);
+  }
+
   free(pixels);
+  free(newPixels);
   return 0;
 }
