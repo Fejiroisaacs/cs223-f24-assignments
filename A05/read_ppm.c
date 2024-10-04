6a61b9724e58281ed8b69868e72fe115ea754c9f
Oghenefejiro Anigboro <fejiroisaac@gmail.com>
2024-10-03 21:37:29 -0400

    done with 2.2

--- A05/read_ppm.c
+++ A05/read_ppm.c
@@ -1,12 +1,13 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Oghenefejiro Anigboro
+ * Date: 09/30/2024
+ * Description: program which reads a ppm file
  ---------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "read_ppm.h"
+
 
 // Choose *one* to implement (do not remove the other one!)
 
@@ -15,5 +16,36 @@ struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
 }
 
 struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
-  return NULL;
+  printf("Got here\n");
+  FILE* ppmData = fopen(filename, "rb");
+  
+  if (ppmData == NULL) {
+       perror("Error opening file");
+       return NULL;
+   }
+
+  struct ppm_pixel** binary = NULL;
+  char buff[1000];
+  int index = 0;
+  while(fgets(buff, 100, ppmData)){
+    
+    if(index == 2){
+      *w = (int)buff[0] - (int)'0';
+      *h = (int)buff[2] - (int)'0';
+      //break;
+    }
+    if(index == 3) break;
+    index++;
+  }
+  
+  binary = malloc(sizeof(struct ppm_pixel*)* *w);
+  
+  for(int i = 0; i < *h; i++){
+    binary[i] = malloc(sizeof(struct ppm_pixel)* *h);
+    for(int j = 0; j < *w; j++) fread(&binary[i][j], sizeof(struct ppm_pixel), 1, ppmData);
+  }
+  
+  fclose(ppmData);
+  return binary;
+  
 }
--- A05/test_read.c
+++ A05/test_read.c
@@ -1,7 +1,7 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Oghenefejiro Anigboro 
+ * Date: 10/2/2024
+ * Description: Program which reads and prints the values of a binary file
  ---------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,9 +9,20 @@
 
 int main() {
   int w, h;
-  struct ppm_pixel* pixels = read_ppm("feep-raw.ppm", &w, &h);
-
+  
+  struct ppm_pixel** pixels = read_ppm_2d("feep-raw.ppm", &w, &h);
   // todo: print out the grid of pixels
+  for(int i = 0; i < w; i++){
+   for(int j = 0; j < h; j++){
+      printf("(%hhu,%hhu,%hhu) ", pixels[i][j].red, pixels[i][j].green, pixels[i][j].blue);
+    }
+    printf("\n");
+  }
+  
+  for(int i = 0; i < w; i++){
+    free(pixels[i]);
+  }
+
   free(pixels);
   return 0;
 }
