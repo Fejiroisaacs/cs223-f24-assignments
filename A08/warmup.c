ecd841c9a1fd7ff91f8fe4b0073a6d657fe0349e
Oghenefejiro Anigboro <fejiroisaac@gmail.com>
2024-10-26 18:06:21 -0400

    done with warmpu

--- A08/warmup.c
+++ A08/warmup.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
+#include  <string.h>
+#include  <sys/types.h>
+#include <unistd.h>
 
 int main() {
+  pid_t pid;
+  
+  printf("%d] A0\n", getpid());
+  fflush(stdout);
+  pid = fork();
+
+  if (pid == 0){
+    printf("%d] B1\n", getpid());
+    fflush(stdout);
+  } else {
+    printf("%d] B0\n", getpid());
+    fflush(stdout);
+    pid = fork();
+    if (pid == 0){
+      printf("%d] C1\n", getpid());
+      fflush(stdout);
+    } else {
+      printf("%d] C0\n", getpid());
+      fflush(stdout);
+    }
+  }
+  
+  printf("%d] Bye\n", getpid());
+
   return 0;
+
 }
